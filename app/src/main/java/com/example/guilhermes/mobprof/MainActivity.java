3c1b9c8800c7b778498e255aaef8b123849d5853
silvaguilherme <silva.guilherme@outlook.com>
2017-06-22 16:47:54 -0300

    Versão 2 - Guilherme

--- app/src/main/java/com/example/guilhermes/mobprof/MainActivity.java
+++ app/src/main/java/com/example/guilhermes/mobprof/MainActivity.java
@@ -80,17 +80,19 @@ public class MainActivity extends AppCompatActivity
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        if (id == R.id.nav_camera) {
+        if (id == R.id.nav_dashboard) {
             // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
+        } else if (id == R.id.nav_perfil) {
 
-        } else if (id == R.id.nav_slideshow) {
+        } else if (id == R.id.nav_criacao_prova) {
 
-        } else if (id == R.id.nav_manage) {
+        } else if (id == R.id.nav_mensagem) {
 
-        } else if (id == R.id.nav_share) {
+        } else if (id == R.id.nav_calendario) {
 
-        } else if (id == R.id.nav_send) {
+        } else if (id == R.id.nav_des_aluno) {
+
+        } else if (id == R.id.nav_des_turma) {
 
         }
 
